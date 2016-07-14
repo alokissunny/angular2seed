c22dda689751289806146267135848e7ed6ef88e
Alok Srivastava <Alok.Srivastava@guavus.com>
2016-07-14 21:59:23 +0530

    Revert "FL: change request+ gulp"
    This reverts commit 5c120cf689f7423bf63999690a38f027a7f0fe61.

---
+++ angular2_Seed/src/app/angular2-seed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'angular2-seed-app',
+  templateUrl: 'angular2-seed.component.html',
+  styleUrls: ['angular2-seed.component.css']
+})
+export class Angular2SeedAppComponent {
+  title = 'angular2-seed works!';
+}
